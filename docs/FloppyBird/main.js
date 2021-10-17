c2df2e8801b33fdc0daa18c54be9b7155dd2f327
William Propp <33852102+williamPropp@users.noreply.github.com>
2021-10-17 02:17:36 -0700

    pipes, collision, and scoring

--- docs/FloppyBird/main.js
+++ docs/FloppyBird/main.js
@@ -15,6 +15,9 @@ const G = {
 
   FALLSPEED: 3,
   JUMPHEIGHT: 1.5,
+
+  PIPEWIDTH: 10,
+  GAPSIZE: 40,
 };
 
 
@@ -62,34 +65,27 @@ let player;
 * @type  { Star [] }
 */
 let stars;
-///////////////////Bill's Code
+
 /**
 * @typedef {{
 * pos: Vector,
+* topHeight: Number,
 * speed: number
 * }} Pipe
 */
-
+  
 /**
 * @type  { Pipe [] }
 */
 let pipes;
-/**
-* @typedef {{
-* pos: Vector,
-* }} Hole
-*/
-/**
-* @type  { Pipe [] }
-*/
-let holes;
-////////////////////Bill's code
+
+let scored = false;
 
 options = {
   viewSize: { x: G.WIDTH, y: G.HEIGHT },
   isPlayingBgm: true,
   isReplayEnabled: true,
-  //seed: 42069,  // XD!!! 
+  seed: 42069,  // XD!!! 
   theme: "dark"
 };
 
@@ -107,28 +103,21 @@ function update() {
         speed: 0.2
       })
     }
-    ///////////////////Bill's
-    let i;
-    pipes = times(3, () => {
+
+    let i = 0;
+    pipes = times(4, () => {
       const posX = 96 + i * 40;
-      i++
-      const posY = 75;
+      i++;
+
+      const h = pipeRandomizer();
+
       return {
-        pos: vec(posX, posY),
-        speed: .15
+        pos: vec(posX, 0),
+        topHeight: h,
+        // speed: .15
+        speed: 1
       };
     });
-    i = 0;
-    holes = times(3, () => {
-      const posX = 96 + i * 40;
-      i++
-      const posY = Math.random() * 120;
-      return {
-        pos: vec(posX, posY),
-        speed: .15
-      };
-    });
-    ////////////////////////Bill's
   }
 
   stars.forEach((s) => {
@@ -137,32 +126,39 @@ function update() {
     color("light_black");
     box(s.pos, 1);
   });
-  ///////////////////////////Bill's
-  pipes.forEach((s) => {
-    s.pos.x -= s.speed;
-    if (s.pos.x < 0) {
-      s.pos.x = 120
+  pipes.forEach((p) => {
+    p.pos.x -= p.speed;
+    if (p.pos.x < -G.PIPEWIDTH) {
+      p.pos.x = 150;
+      p.topHeight = pipeRandomizer();
+      scored = false;
     }
-    color("black");
-    box(s.pos, 8, 150);
-  });
-  holes.forEach((s) => {
-    s.pos.x -= s.speed;
-    if (s.pos.x < 0) {
-      s.pos.x = 120
+
+    if (p.pos.x < player.pos.x - 3 && !scored) {
+      addScore(1,player.pos);
+      play("coin");
+      scored = true;
     }
-    color("black");
-    box(s.pos, 8, 40);
+
+    if(pipeCollisionTest(p)) {
+      play("explosion");
+      end();
+    }
+
+    color("blue");
+    rect(p.pos.x, 0, G.PIPEWIDTH, p.topHeight);
+    rect(p.pos.x, p.topHeight + G.GAPSIZE, G.PIPEWIDTH, 150);
   });
-  ////////////////////////////Bill's
+
   // render bird
   color("black");
   // add animation : flapping -Larry
   if (input.isPressed) {
-    char("a", player.pos);
+    char("a", player.pos).isColliding.rect.blue;
   } else {
     char("b", player.pos);
   }
+  
 
 
   // grass border
@@ -174,12 +170,15 @@ function update() {
   //Since there are two characters, Make sure to check both of the collision -Larry
   if (isCollidingWithGrass) {
     // if fall on grass, end game
-    console.log("hit")
-    end("You Died");
+    console.log("hit");
+    color("red");
+    particle(player.pos);
+    play("explosion");
+    end();
   }
 
   // simulate gravity
-  if (player.pos.y + player.vel.y >= 0) {//added boarder for the bird -Larry
+  if (player.pos.y + player.vel.y >= 0) {//added border for the bird -Larry
     player.pos.y += player.vel.y;
   } else {
     player.vel.y = 0;               //now it won't fly out of the screen
@@ -191,4 +190,20 @@ function update() {
     play("laser");
   }
 
+}
+
+function pipeRandomizer(){
+  return Math.floor(Math.random() * (G.GRASSBORDER - G.GAPSIZE - 20)) + 20;
+}
+
+function pipeCollisionTest(pipe){
+  if(pipe.pos.x < (player.pos.x + 2) && pipe.pos.x > (player.pos.x - 3)){
+    if(player.pos.y <= (pipe.topHeight/* + G.GAPSIZE*/) || player.pos.y >= (pipe.topHeight + G.GAPSIZE)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
